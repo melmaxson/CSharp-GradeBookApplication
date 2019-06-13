478655c724af481aed9417a0e45ce70ddb39afe0
Melody Maxson <melody.maxson@pressganey.com>
2019-06-13 15:55:50 -0400

    Override RankedGradeBook's GetLetterGrade

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,4 +1,7 @@
 ﻿using GradeBook.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace GradeBook.GradeBooks
 {
@@ -7,6 +10,51 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+
+        //public int studentRank;
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            int letterGradeGroupSize = (int)Math.Ceiling(Students.Count * .2);
+            
+            if (Students.Count < 5)
+                throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work.");
+            else
+            {
+                int rank = CalculateStudentRank(averageGrade);
+ 
+                if (rank <= letterGradeGroupSize)
+                    return 'A';
+                else if (rank <= (2 * letterGradeGroupSize))
+                    return 'B';
+                else if (rank <= (3 * letterGradeGroupSize))
+                    return 'C';
+                else if (rank <= (4 * letterGradeGroupSize))
+                    return 'D';
+                else
+                    return 'F';
+            }
+        }
+
+        int CalculateStudentRank(double averageGrade)
+        {
+            int studentRank = 1;
+
+            foreach (var student in Students)
+            {
+                
+                if (student.AverageGrade > averageGrade)
+                {
+                    studentRank += 1;
+                    //else if (student.AverageGrade > averageGrade)
+                    //  studentsAbove += 1;
+                    //else if (student.AverageGrade == averageGrade)
+                    //    sameGrade += 1;
+
+                }
+            }
+            return studentRank;
         }
 
     }
--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -5,8 +5,8 @@ namespace GradeBook.GradeBooks
     public class StandardGradeBook : BaseGradeBook
     {
         public StandardGradeBook(string name) : base(name)
-    {
-        Type = GradeBookType.Standard;
-    }
+        {
+            Type = GradeBookType.Standard;
+        }
     }
 }
