cbe1fbadf22e32eff6fdfbfd4bfffa5339777021
Melody Maxson <melody.maxson@pressganey.com>
2019-06-18 10:26:50 -0400

    Update RankedGradeBook.cs
    Override Statistics

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -57,5 +57,21 @@ namespace GradeBook.GradeBooks
             return studentRank;
         }
 
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+                Console.WriteLine("Ranked grading requires at least 5 students with grads in order to properly calculate a student's overall grade.");
+            else
+                base.CalculateStatistics();
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+                Console.WriteLine("Ranked grading requires at least 5 students with grads in order to properly calculate a student's overall grade.");
+            else
+                base.CalculateStudentStatistics(name);
+        }
+
     }
 }
